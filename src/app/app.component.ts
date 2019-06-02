af377b7a5d0d1b9321a755be2e4c7309cb70ed8c
Enes <enes.er2709@gmail.com>
2019-06-02 14:41:29 +0200

    Title animation
    we put jquery manually on the angular.json becousae of the fsevents issue, and then we put the animation on the title, now we will begin to the navbar design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'pantinWeb';
+
+    ngOnInit(): void {
+    $(document).ready(() => {
+
+        $('#titleDiv').animate({
+            opacity: 1
+        },2000,function() {});
+
+        setTimeout(function() {
+            $('hr').addClass('grow')
+        }, 1000);
+        
+    });
+  }
 }
